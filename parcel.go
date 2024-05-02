12b58245e412d41d96a7730d1e834132c0195739
Konstantin Lapshin <lapshin.k@gmail.com>
2024-05-02 21:10:38 +0300

    retry 5

--- parcel.go
+++ parcel.go
@@ -36,19 +36,16 @@ func (s ParcelStore) Add(p Parcel) (int, error) {
 func (s ParcelStore) Get(number int) (Parcel, error) {
 	// реализуйте чтение строки по заданному number
 	// здесь из таблицы должна вернуться только одна строка
-	rows, err := s.db.Query("SELECT number,client,status,address,created_at FROM parcel WHERE number = :number", sql.Named("number", number))
+	row := s.db.QueryRow("SELECT number,client,status,address,created_at FROM parcel WHERE number = :number", sql.Named("number", number))
+
+	// заполните объект Parcel данными из таблицы
+	p := Parcel{}
+	err := row.Scan(&p.Number, &p.Client, &p.Status, &p.Address, &p.CreatedAt)
 	if err != nil {
 		return Parcel{}, err
 	}
-	// заполните объект Parcel данными из таблицы
-	p := Parcel{}
-	for rows.Next() {
-		err := rows.Scan(&p.Number, &p.Client, &p.Status, &p.Address, &p.CreatedAt)
-		if err != nil {
-			return Parcel{}, err
-		}
-	}
 	return p, nil
+
 }
 
 func (s ParcelStore) GetByClient(client int) ([]Parcel, error) {
