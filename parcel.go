d7635bdb5269cacbc7a8e4ce3f2129cb78da6c8f
Konstantin Lapshin <lapshin.k@gmail.com>
2024-05-03 11:04:05 +0300

    retry 6

--- parcel.go
+++ parcel.go
@@ -55,6 +55,7 @@ func (s ParcelStore) GetByClient(client int) ([]Parcel, error) {
 	if err != nil {
 		return nil, err
 	}
+	defer rows.Close()
 	// заполните срез Parcel данными из таблицы
 	var res []Parcel
 	for rows.Next() {
